d382ff9257a3a2781f8ffec596af4dc951e0a3a9
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-02-12 11:46:03 +0300

    we use first text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
 }
