246a0817f499478758be4ec99d3f59ea1bcd87fb
Aytac Bulanik <aytacbulanik@hotmail.com>
2025-02-13 16:27:26 +0300

    tabbar added

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ana Ekran"),
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+        ),
       ),
     ),
   );
